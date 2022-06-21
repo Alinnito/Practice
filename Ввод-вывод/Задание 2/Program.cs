17aacde8668a33cf948de06b5d5f2bbc64f2c263
Alinnito <107494829+Alinnito@users.noreply.github.com>
2022-06-21 14:09:04 +0300

    Add files via upload

---
+++ Ввод-вывод/Задание 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static string path = @"C:\Users\user\source\repos\Спасова\-\Ввод-вывод\1.txt";
+        static void Main(string[] args)
+        {
+            using (FileStream fs = File.Create(path)) { }
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    for(int i = 0; i < 5; i++)
+                        sw.WriteLine(i);
+                }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                    Console.WriteLine(sr.ReadLine());
+            }
+            Console.ReadLine();
+        }
+    }
+}
